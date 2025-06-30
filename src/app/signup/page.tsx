afd3d49fe6590fe61ce765b311d7835f3dcddf0e
Mayank Dayal <mayankdl203@gmail.com>
2025-06-30 07:15:52 +0000

    ok we will deal with it later also only show the homepage wihout login p

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,19 +1,14 @@
 'use client';
 
-import { AuthForm } from '@/components/AuthForm';
-import { Footer } from '@/components/Footer';
-import { Header } from '@/components/Header';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function SignupPage() {
-  return (
-    <div className="flex flex-col min-h-dvh bg-transparent text-foreground font-body">
-      <Header />
-      <main className="flex-1 flex items-center justify-center py-12 md:py-20">
-        <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <AuthForm mode="signup" />
-        </div>
-      </main>
-      <Footer />
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/');
+  }, [router]);
+
+  return null;
 }
